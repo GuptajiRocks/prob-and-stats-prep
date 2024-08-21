b213649b302b9332dc19085e4fba515b82a8c6b8
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-21 13:02:35 +0530

    coin prob calc

--- 14labnextprog/2.py
+++ 14labnextprog/2.py
@@ -1,18 +1,32 @@
 import itertools
 
 def coin_toss_probabilities(num_coins):
-  
-
   outcomes = list(itertools.product(['H', 'T'], repeat=num_coins))
-  total_outcomes = len(outcomes)
-
-  probabilities = {}
-  for outcome in outcomes:
-    probabilities[outcome] = 1 / total_outcomes
-
-  return probabilities
-
+  return outcomes
+cc = 0
 num_coins = int(input("Enter the number of coins: "))
-
 probs = coin_toss_probabilities(num_coins)
-print(probs)
+prob_c = 0
+print("For which do you want to calculate probability -> (H/T)?: ")
+sidechoice = str(input("Enter: "))
+if sidechoice == ('H' or 'h'):
+  print(f"Enter the number of {sidechoice} you want probability of: ")
+  coinc = int(input("Enter the value: "))
+  cc = coinc
+  for i in probs:
+    if i.count('H') == coinc:
+      prob_c +=1
+else:
+  print("Invalid input") 
+
+
+total = num_coins**2
+
+print(f"The probability for {cc}, {sidechoice} coins simultaneously is: {cc/total}")
+
+
+
+
+
+
+
