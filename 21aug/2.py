b8dad3106a2b7bce8ee0e3cbbfa227d8a2523036
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-21 11:29:22 +0530

    bayes theorem prob

---
+++ 21aug/2.py
@@ -0,0 +1,11 @@
+pbdwb = 0.93
+pnbwnb = 0.88
+pbr = 0.03
+pnb = 1-pbr
+pbrwnb = 1 - pnbwnb 
+
+pbd = (pbdwb*pbr) + (pbrwnb*pnb)
+
+pbrwbd = (pbdwb * pbr)/pbd
+
+print(round(pbrwbd,4))
---
+++ 21aug/3.py
@@ -0,0 +1,5 @@
+pwhenf = 0.65
+pdiswhenjp =0.55
+pdiswhenboth = 0.85
+
+print(pdiswhenboth)
