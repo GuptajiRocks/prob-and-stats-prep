1a93c95a5f6c772281dc7d3e379d20aacdea24e7
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-28 12:13:53 +0530

    need to try 3rd ques

--- 28aug/3.py
+++ 28aug/3.py
@@ -1,14 +1,15 @@
 x = 2000
 
 dd = {1:0.3, 2:0.4, 3:0.2, 4:0.1}
+dk = list(dd.keys())
 
 arrvall = []
 for i in dd:
-    arrvall.append(dd[i]*2000)
+    arrvall.append(int(dd[i]*2000))
 fullar=[]
 for i in arrvall:
-    for j in range(int(i)):
-        fullar.append()
+    for j in range(i):
+        fullar.append(dk[i])
 
 print(fullar)
 
